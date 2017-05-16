02fe1fd64f6a0028de37b44147a244cadd6238a1
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-05-16 23:21:25 +0530

    that was a mistake

--- src/components/List.js
+++ src/components/List.js
@@ -6,7 +6,7 @@ class List extends Component {
             <div className="Row">
                 <div className="Index">1</div>
                 <div className="Name">Abhishek</div>
-                <div className="MobileNumber">+919560814780</div>
+                <div className="MobileNumber">+919999999999</div>
             </div>
         );
     }
