962d314eaa452663c48bb7010a9b6aa1972bc06d
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-05-17 15:42:39 +0530

    formatting

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,72 +1,88 @@
 import React, {Component} from 'react';
 import '../css/AddContact.css';
-import {
-    connect
-} from 'react-redux';
+import {connect} from 'react-redux';
 
 import addContacts from '../redux/actions/contactActions.js';
 
 import mapStateToProps from '../util/mapStateToProps.js';
 
-
 class AddContact extends Component {
 
-     constructor(props) {
-         super(props);
-         this.state = {
-             name: '',
-             phoneNumber:''
-         };
-         this.handleChange = this.handleChange.bind(this);
-         this.handleSubmit = this.handleSubmit.bind(this);
-     }
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            phoneNumber: ''
+        };
+        this.handleChange = this
+            .handleChange
+            .bind(this);
+        this.handleSubmit = this
+            .handleSubmit
+            .bind(this);
+    }
 
-     handleChange(event) {
-         if (event.target.id === 'name') {
-             this.setState({
-                 name: event.target.value
-             });
-         } else {
-             this.setState({
-                 phoneNumber: event.target.value
-             });
-         }
-     }
+    handleChange(event) {
+        if (event.target.id === 'name') {
+            this.setState({name: event.target.value});
+        } else {
+            this.setState({phoneNumber: event.target.value});
+        }
+    }
 
-     handleSubmit(event) {
-         event.preventDefault();
-         
-         let newContact = {
+    handleSubmit(event) {
+        event.preventDefault();
 
-             name: this.state.name,
-             phoneNumber: this.state.phoneNumber,
-             index:this.props.list.length + 1
+        let newContact = {
 
-         };
-         
-         let {dispatch} = this.props;
-         dispatch(addContacts(newContact));         
-         this.props.router.push('/');
-     }
+            name: this.state.name,
+            phoneNumber: this.state.phoneNumber,
+            index: this.props.list.length + 1
+
+        };
+
+        let {dispatch} = this.props;
+        dispatch(addContacts(newContact));
+        this
+            .props
+            .router
+            .push('/');
+    }
 
     render() {
         return (
             <div className='FormContainer'>
-            
-                <h2>Add</h2>
-            
-                <form onSubmit={this.handleSubmit} className='ContactForm'>
-                    
-                    <label htmlFor='name' className='namelabel'>Name</label>
-                    <input type="text" id='name' placeholder='Harry Potter' autoComplete={true} max='25' min='1' value={this.state.name} onChange={this.handleChange} required/>
-                    
-                    <label htmlFor='mobile' className='mobilelabel' >Mobile Number</label>
-                    <input type="tel" id='mobile' placeholder='+91-XXX-XXX-XXX' autoComplete={true} value={this.state.phoneNumber} onChange={this.handleChange} required/>
 
-                    <input type="submit" id='submitAddContact' value="Add Contact" />
-                
+                <h2>Add</h2>
+
+                <form onSubmit={this.handleSubmit} className='ContactForm'>
+
+                    <label htmlFor='name' className='namelabel'>Name</label>
+                    <input
+                        type="text"
+                        id='name'
+                        placeholder='Harry Potter'
+                        autoComplete={true}
+                        max='25'
+                        min='1'
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        required/>
+
+                    <label htmlFor='mobile' className='mobilelabel'>Mobile Number</label>
+                    <input
+                        type="tel"
+                        id='mobile'
+                        placeholder='+91-XXX-XXX-XXX'
+                        autoComplete={true}
+                        value={this.state.phoneNumber}
+                        onChange={this.handleChange}
+                        required/>
+
+                    <input type="submit" id='submitAddContact' value="Add Contact"/>
+
                 </form>
-            
+
             </div>
         );
     }
