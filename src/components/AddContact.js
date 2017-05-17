dc8d5cb15d753f20ef8aad44f6a6237638af5b41
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-05-17 13:31:23 +0530

    need to eject to add decorators

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,22 +1,66 @@
 import React, {Component} from 'react';
+import '../css/AddContact.css';
+import {
+    connect
+} from 'react-redux';
+
+
 
 class AddContact extends Component {
 
-    onclick(){
-        
-    }
+     constructor(props) {
+         super(props);
+         this.state = {
+             name: '',
+             phoneNumber:''
+         };
+
+         this.handleChange = this.handleChange.bind(this);
+         this.handleSubmit = this.handleSubmit.bind(this);
+     }
+
+     handleChange(event) {
+         if (event.target.id === 'name') {
+             this.setState({
+                 name: event.target.value
+             });
+         } else {
+             this.setState({
+                 phoneNumber: event.target.value
+             });
+         }
+     }
+
+     handleSubmit(event) {
+         let newContact = {
+
+             name: this.state.name,
+             phoneNumber: this.state.phoneNumber
+
+         }
+         
+         console.log(newContact);
+
+         event.preventDefault();
+     }
 
     render() {
         return (
-            <div className='AddContactContainer'>
-            <h1>Add</h1>
-            <form action={this.onclick}></form>
+            <div className='FormContainer'>
+            
+            <h2>Add</h2>
+            
+            <form onSubmit={this.handleSubmit} className='ContactForm'>
                 
-                <input type="number" autoComplete={true}/>
+                <label htmlFor='name' className='namelabel'>Name</label>
+                <input type="text" id='name' placeholder='Harry Potter' autoComplete={true} max='25' min='1' value={this.state.name} onChange={this.handleChange} required/>
                 
-                <input type="text"/>
+                <label htmlFor='mobile' className='mobilelabel' >Mobile Number</label>
+                <input type="tel" id='mobile' placeholder='+91-XXX-XXX-XXX' autoComplete={true} value={this.state.phoneNumber} onChange={this.handleChange} required/>
 
-                <button onClick={this.onclick}>Submit</button>
+                <input type="submit" id='submitAddContact' value="Add Contact" />
+            </form>
+            
             </div>
         );
     }
